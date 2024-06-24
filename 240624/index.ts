d21b37acc9c8f9be85f45c42f0fb68413e7888e6
jodasom <jodasom0216@gmail.com>
2024-06-24 15:36:34 +0900

    타입스크립트 생성

---
+++ 240624/index.ts
@@ -0,0 +1,24 @@
+// number
+// string
+// boolean
+// null
+// undefined
+// any
+
+let a: number = 3
+let b : string = 'love'
+
+let c: any = 4
+c="dfssd"
+
+let d: number| string = 'dfss'
+d=3
+
+let e:string[] = ['apple','mongo']
+
+let f:number[] = [1,2]
+
+function addNumber (a:number, b:number):number{
+    return a+b
+}
+addNumber(3,7)
