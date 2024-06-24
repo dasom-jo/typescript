4b8e689fc2231e784786559b323ec146f8279809
jodasom <jodasom0216@gmail.com>
2024-06-24 17:38:36 +0900

    err

---
+++ 240624/index.js
@@ -0,0 +1,18 @@
+// number
+// string
+// boolean
+// null
+// undefined
+// any
+var a = 3;
+var b = 'love';
+var c = 4;
+c = "dfssd";
+var d = 'dfss';
+d = 3;
+var e = ['apple', 'mongo'];
+var f = [1, 2];
+function addNumber(a, b) {
+    return a + b;
+}
+addNumber(3, 7);
